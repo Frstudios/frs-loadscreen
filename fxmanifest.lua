ffbdfab4ca6c9b3c89c4df8bd1d3785b02f59555
Frstudios <frstudios24@gmail.com>
2025-01-06 19:43:39 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'haydo.lua'
+description 'Fr studios'
+version '1.0.0'
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/assets/video.mp4',
+    'html/assets/audio.mp3',
+    'html/script.js',
+    'html/**/*',           
+    'html/assets/**/*',    
+    'html/images/**/*',   
+    'html/minigames/**/*', 
+}
+
+client_script 'client.lua'
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
